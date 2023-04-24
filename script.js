456bb4cef476011d00cb404eba80d3a31f514b5c
GuiJg <j.gu1lh3rm325@gmail.com>
2023-04-24 14:56:29 -0300

    second

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ setTimeout(() => {
     document.querySelectorAll(".typing-text")[0].style.borderRightColor = "transparent"
     document.querySelector("h1").style.animation = "typing 2s steps(34), cursor .4s step-end infinite alternate"
   }, 2000)
+
+setTimeout(() => {
+    document.querySelector(".typing-front").style.opacity = 1
+    document.querySelectorAll(".typing-text")[1].style.borderRightColor = "transparent"
+    document.querySelector(".typing-front").style.animation = "typing 2s steps(34), cursor .4s step-end infinite alternate"
+  }, 4200) 
