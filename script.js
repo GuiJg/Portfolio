097c1f3db62304c35b54dcb640e89246a4b6597a
GuiJg <j.gu1lh3rm325@gmail.com>
2023-06-02 18:41:53 -0300

    sixth

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ setTimeout(() => {
     document.querySelectorAll(".typing-text")[1].style.borderRightColor = "transparent"
     document.querySelector(".typing-front").style.animation = "typing 2s steps(34), cursor .4s step-end infinite alternate"
   }, 4200) 
+
+//navbar mobile
+const menuButton = document.querySelector('.menu-button');
+const headerMenu = document.querySelector('.header-menu');
+  
+  menuButton.addEventListener('click', function() {
+      headerMenu.classList.toggle('active');
+  });
