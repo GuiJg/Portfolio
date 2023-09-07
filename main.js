cb0621716dc93e443efc4a2d54d02a25f40adeed
GuiJg <j.gu1lh3rm325@gmail.com>
2023-09-07 12:53:07 -0300

    portfolio update

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-//navbar mobile
+//mudança de navbar responsivo para mobile
 const menuButton = document.querySelector('.toggle');
 const headerMenu = document.querySelector('.header-menu');
   
-  menuButton.addEventListener('click', function() {
-      headerMenu.classList.toggle('active');
-  });
+menuButton.addEventListener('click', function() {
+    headerMenu.classList.toggle('active');
+});
 
-  window.addEventListener('scroll', function(){
+//Mudança de transparência do botão (back-to-the-top)
+window.addEventListener('scroll', function(){
 
-    const button = document.querySelector('.btn-content');
-    let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  const button = document.querySelector('.btn-content');
+  let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   
-    if (scrollPosition > 100) {
-      button.style.opacity = '1';
-    } else {
-      button.style.opacity = '0';
-    }
-  });
+  if (scrollPosition > 100) {
+    button.style.opacity = '1';
+  } else {
+    button.style.opacity = '0';
+  }
+});
 
-//navbar transparent
+//Mudança de transparência do header
 const menu = document.getElementById("portfolio"); 
 window.addEventListener("scroll", function () {
     
@@ -32,5 +33,18 @@ window.addEventListener("scroll", function () {
     for (let i = 0; i < document.querySelectorAll(".header-link").length; i++) {
         
     }
+  }
+});
+
+//Mudança de transparência dos botões das redes sociais
+window.addEventListener('scroll', function(){
+
+  const button = document.querySelector('.social-fixed');
+  let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollPosition > 100) {
+    button.style.opacity = '1';
+  } else {
+    button.style.opacity = '0';
   }
 });
